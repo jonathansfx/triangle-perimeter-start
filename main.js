2f65399e4bbfe31dc2ca7b0a656ce374466deb77
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-02-08 11:21:20 -0700

    work on js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Triangle Perimeter Assignment Start Code
+// button event listener
+let btn = document.getElementById("button");
+
+btn.addEventListener("click", btnClicked);
+function btnClicked() {
+  // html variables
+  let xAinput = +document.getElementById("xA-input").value;
+  let yAinput = +document.getElementById("yA-input").value;
+  let xBinput = +document.getElementById("xB-input").value;
+  let yBinput = +document.getElementById("yB-input").value;
+  let xCinput = +document.getElementById("xC-input").value;
+  let yCinput = +document.getElementById("yC-input").value;
+}
