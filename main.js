e6116a38f0f1e8287c90aef064b8c5de439b5723
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-02-13 10:54:02 -0700

    finish

--- main.js
+++ main.js
@@ -4,11 +4,27 @@ let btn = document.getElementById("button");
 
 btn.addEventListener("click", btnClicked);
 function btnClicked() {
-  // html variables
-  let xAinput = +document.getElementById("xA-input").value;
-  let yAinput = +document.getElementById("yA-input").value;
-  let xBinput = +document.getElementById("xB-input").value;
-  let yBinput = +document.getElementById("yB-input").value;
-  let xCinput = +document.getElementById("xC-input").value;
-  let yCinput = +document.getElementById("yC-input").value;
+  // INPUT
+  let xA = +document.getElementById("xA-input").value;
+  let yA = +document.getElementById("yA-input").value;
+  let xB = +document.getElementById("xB-input").value;
+  let yB = +document.getElementById("yB-input").value;
+  let xC = +document.getElementById("xC-input").value;
+  let yC = +document.getElementById("yC-input").value;
+
+  // PROCESS
+  let AB = dist(xA, yA, xB, yB);
+  let AC = dist(xA, yA, xC, yC);
+  let BC = dist(xB, yB, xC, yC);
+
+  // OUTPUT
+  document.getElementById("abOutput").innerHTML = AB;
+  document.getElementById("acOutput").innerHTML = AC;
+  document.getElementById("bcOutput").innerHTML = BC;
+  document.getElementById("pOutput").innerHTML = AB + AC + BC;
+}
+
+function dist(x1, y1, x2, y2) {
+  let distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return distance;
 }
